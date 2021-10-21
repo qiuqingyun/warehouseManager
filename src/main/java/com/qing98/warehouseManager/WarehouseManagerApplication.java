e0c8e04887811cff728c315472e663f860d47476
Qingyun Qiu <qiuqingyun98@outlook.com>
2021-10-21 18:58:23 +0800

    640K ought to be enough for anybody

---
+++ src/main/java/com/qing98/warehouseManager/WarehouseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.qing98.warehouseManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagerApplication.class, args);
+	}
+
+}
